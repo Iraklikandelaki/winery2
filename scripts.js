d4283a9358868277234037f46d31dd614388595e
Iraklikandelaki <72970018+Iraklikandelaki@users.noreply.github.com>
2021-02-11 14:12:47 +0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -162,22 +162,21 @@ scene.add( cube2 );
 	
   
 
- // Create a circle around the mouse and move it
-	// The sphere has opacity 0
-	const listener = new THREE.AudioListener();
-	camera.add( listener );
+ 
+// 	const listener = new THREE.AudioListener();
+// 	camera.add( listener );
 	
-	// create a global audio source
-	const sound = new THREE.Audio( listener );
+// 	// create a global audio source
+// 	const sound = new THREE.Audio( listener );
 	
-	// load a sound and set it as the Audio object's buffer
-	const audioLoader = new THREE.AudioLoader();
-	audioLoader.load( './sonata.mp3', function( buffer ) {
-		sound.setBuffer( buffer );
-		sound.setLoop( true );
-		sound.setVolume( 0.5 );
-		sound.play();
-	});
+// 	// load a sound and set it as the Audio object's buffer
+// 	const audioLoader = new THREE.AudioLoader();
+// 	audioLoader.load( './sonata.mp3', function( buffer ) {
+// 		sound.setBuffer( buffer );
+// 		sound.setLoop( true );
+// 		sound.setVolume( 0.5 );
+// 		sound.play();
+// 	});
 
 
 
